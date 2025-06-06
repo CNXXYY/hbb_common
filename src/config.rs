0da8fdc65d395d8f4869af7244f99012d73fa144
CNXXYY <29110101+CNXXYY@users.noreply.github.com>
2025-06-06 19:47:51 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -99,7 +99,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RS_PUB_KEY: &str = "VNKMJa6ZW6HeN9siFmke5QDY57oB1bggJazhcq7k1ak";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
